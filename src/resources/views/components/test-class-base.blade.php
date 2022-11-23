2f6da57232f618b8909fd676db8ff76633ebfd83
mana0902 <todamana0902@gmail.com>
2022-11-23 18:08:53 +0900

    classbase component

---
+++ src/resources/views/components/test-class-base.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Be present above all else. - Naval Ravikant -->
+classbase component
+<div >{{ $classBaseMessage }}</div>
+<div >{{ $defaultMessage }}</div>
+</div>
--- src/resources/views/tests/component-test1.blade.php
+++ src/resources/views/tests/component-test1.blade.php
@@ -3,4 +3,6 @@
 component test1
 <x-tests.card title="タイトル" content="本文" :message="$message"/>
 <x-tests.card class='bg-slate-400' title="タイトル" content="本文" :message="$message"/>
+<x-test-class-base classBaseMessage="メッセージです"/>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値変更"/>
 </x-tests.app>
