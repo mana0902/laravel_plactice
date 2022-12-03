32b100f3e3b2c384bd58ec506f9854ddf38a7ed5
mana0902 <todamana0902@gmail.com>
2022-12-03 10:05:39 +0900

    owners edit/update

--- src/resources/views/admin/owners/index.blade.php
+++ src/resources/views/admin/owners/index.blade.php
@@ -13,7 +13,7 @@
                         <x-flash-message statu="info" />
                         <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                         <div class="flex justify-end mb-4">
-                            <button onclick="location.href='{{route('admin.owners.create')}}'" class=" ml-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                            <button onclick="location.href='{{route('admin.owners.create')}}'" class=" ml-auto text-white bg-indigo-500 border-0 mt-2 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
                         </div>
                         <table class="table-auto w-full text-left whitespace-no-wrap">
                             <thead>
@@ -21,7 +21,7 @@
                                 <th class="px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
                                 <th class="px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
                                 <th class="px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
-                                <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                <th class="w-20 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                             </tr>
                             </thead>
                             <tbody>
@@ -31,7 +31,7 @@
                                 <td class="px-4 py-3">{{$owner->email}}</td>
                                 <td class="px-4 py-3">{{$owner->created_at->diffForHumans()}}</td>
                                 <td class="w-10 text-center">
-                                <input name="plan" type="radio">
+                                <button type="submit" onclick="location.href='{{ route('admin.owners.edit',['owner'=>$owner->id])}}'" class=" text-white bg-blue-500 border-0 py-1.5 px-4 focus:outline-none hover:bg-indigo-600 rounded ">編集</button>
                                 </td>
                             </tr>
                             @endforeach
