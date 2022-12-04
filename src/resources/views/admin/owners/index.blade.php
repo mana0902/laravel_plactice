6241334555b84e03a56462eef7d0259a81baad1b
mana0902 <todamana0902@gmail.com>
2022-12-04 10:53:31 +0900

    edit route/view

--- src/resources/views/admin/owners/index.blade.php
+++ src/resources/views/admin/owners/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="md:p-6 text-gray-900">
                     <section class="text-gray-600 body-font">
                         <x-flash-message statu="session('status')" />
                         <div class="lg:w-2/3 w-full mx-auto overflow-auto">
@@ -18,11 +18,11 @@
                         <table class="table-auto w-full text-left whitespace-no-wrap">
                             <thead>
                             <tr>
-                                <th class="px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
-                                <th class="px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
-                                <th class="px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
-                                <th class="px-2 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
-                                <th class="px-2 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                <th class="md:px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
+                                <th class="md:px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
+                                <th class="md:px-4 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
+                                <th class="md:px-2 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                <th class="md:px-2 py-5 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                             </tr>
                             </thead>
                             <tbody>
